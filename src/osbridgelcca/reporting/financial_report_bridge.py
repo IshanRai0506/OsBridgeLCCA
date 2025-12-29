5d1618b3f063555775c9017470707c32bdc1d0ca
Ishan Rai <ishanrai646@gmail.com>
2025-12-29 11:32:03 +0530

    update

--- src/osbridgelcca/reporting/financial_report_bridge.py
+++ src/osbridgelcca/reporting/financial_report_bridge.py
@@ -1,16 +1,20 @@
+# financial_report_bridge.py
+
+from pathlib import Path
 from osbridgelcca.simple_report.financial_report_generator import FinancialReportGenerator
 
-def generate_financial_pdf(financial_data: dict):
+def generate_financial_pdf(financial_data: dict, time_cost=None, output_dir=None, logo_path=None):
     generator = FinancialReportGenerator()
 
     mapped = {
-        "DISCOUNT_RATE": financial_data["Discount Rate(Inflation Adjusted)"],
-        "INFLATION_RATE": financial_data["Inflation Rate"],
-        "INTEREST_RATE": financial_data["Interest Rate"],
-        "INVESTMENT_RATIO": financial_data["Investment Ratio"],
-        "DESIGN_LIFE": financial_data["Design Life"],
-        "CONSTR_TIME": financial_data["Time for Construction of Base Project"],
-        "ANALYSIS_PERIOD": financial_data["Analysis Period"],
+        "Analysis Period": financial_data.get("Analysis Period"),
+        "Design Life": financial_data.get("Design Life"),
+        "Discount Rate(Inflation Adjusted)": financial_data.get("Discount Rate(Inflation Adjusted)"),
+        "Inflation Rate": financial_data.get("Inflation Rate"),
+        "Interest Rate": financial_data.get("Interest Rate"),
+        "Investment Ratio": financial_data.get("Investment Ratio"),
+        "Time for Construction of Base Project": financial_data.get("Time for Construction of Base Project"),
+        "TIME_COST": time_cost
     }
 
-    return generator.generate(mapped)
+    return generator.generate(data=mapped, output_dir=output_dir, logo_path=logo_path)
