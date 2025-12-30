45f2cf65e5876d0699ba5d006c1fb941f848267d
Ishan Rai <ishanrai646@gmail.com>
2025-12-31 02:13:02 +0530

    fix

--- src/osbridgelcca/reporting/financial_report_generator.py
+++ src/osbridgelcca/reporting/financial_report_generator.py
@@ -7,21 +7,21 @@ class FinancialReportGenerator:
         self.output_dir = output_dir
         self.output_dir.mkdir(parents=True, exist_ok=True)
 
-    def generate(self, data, time_cost, filename):
+    def generate(self, data, time_cost, logo_path, filename):
         tex = self.template.read_text()
 
-        for key, val in data.items():
-            tex = tex.replace(f"<<{key}>>", str(val))
-
+        # replace placeholders
+        tex = tex.replace("<<LOGO_PATH>>", logo_path)
+        for key, value in data.items():
+            tex = tex.replace(f"<<{key}>>", str(value))
         tex = tex.replace("<<TIME_COST>>", str(time_cost))
 
         tex_file = self.output_dir / f"{filename}.tex"
+        pdf_file = self.output_dir / f"{filename}.pdf"
+
         tex_file.write_text(tex)
 
-        subprocess.run(
-            ["pdflatex", "-interaction=nonstopmode", tex_file.name],
-            cwd=self.output_dir
-        )
+        # compile TeX
+        subprocess.run(["pdflatex", str(tex_file.name)], cwd=self.output_dir)
 
-        return str(self.output_dir / f"{filename}.pdf")
-
+        return pdf_file
